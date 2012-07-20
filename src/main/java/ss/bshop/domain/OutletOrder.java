5ef449e3c44ff0f9ec53c991a5d397791a814758
unknown <User@.(none)>
2012-07-20 20:59:05 +0300

    first entities

--- src/main/java/ss/bshop/domain/OutletOrder.java
+++ src/main/java/ss/bshop/domain/OutletOrder.java
@@ -1,5 +1,139 @@
 package ss.bshop.domain;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class OutletOrder {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String type; // order-return
+	
+	@Column
+	private byte discount; // order-return
+	
+	@Column
+	private double payment; // order-return
+	
+	@Column
+	private Visit visit; // order-return
+	
+	@Column
+	private List<OutletOrderStructure> structure; // order-return
+
+	
+	//getters and setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public byte getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(byte discount) {
+		this.discount = discount;
+	}
+
+	public double getPayment() {
+		return payment;
+	}
+
+	public void setPayment(double payment) {
+		this.payment = payment;
+	}
+
+	public Visit getVisit() {
+		return visit;
+	}
+
+	public void setVisit(Visit visit) {
+		this.visit = visit;
+	}
+
+	public List<OutletOrderStructure> getStructure() {
+		return structure;
+	}
+
+	public void setStructure(List<OutletOrderStructure> structure) {
+		this.structure = structure;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + discount;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(payment);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result
+				+ ((structure == null) ? 0 : structure.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((visit == null) ? 0 : visit.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OutletOrder other = (OutletOrder) obj;
+		if (discount != other.discount)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (Double.doubleToLongBits(payment) != Double
+				.doubleToLongBits(other.payment))
+			return false;
+		if (structure == null) {
+			if (other.structure != null)
+				return false;
+		} else if (!structure.equals(other.structure))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		if (visit == null) {
+			if (other.visit != null)
+				return false;
+		} else if (!visit.equals(other.visit))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 
 }
