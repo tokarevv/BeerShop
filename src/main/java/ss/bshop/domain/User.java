2ce9cf66e9d1bbadd40c4bb9e5e80e892717c427
nick <nick@eMachina>
2012-07-20 21:20:52 +0300

    добавил юзера

--- src/main/java/ss/bshop/domain/User.java
+++ src/main/java/ss/bshop/domain/User.java
@@ -1,5 +1,64 @@
 package ss.bshop.domain;
 
-public class User {
+import javax.persistence.*;
 
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String login;
+    
+    @Column
+    private String fullname;
+
+    @Column
+    private String password;
+
+    @Column
+    private String position;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+    
+    
 }
