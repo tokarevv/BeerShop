c8fe4458ab1e89e7f7f4c887afb8456ef74e3b38
unknown <User@.(none)>
2012-07-23 17:43:34 +0300

    1

--- src/test/java/ss/bshop/domain/ArticleDAOTest.java
+++ src/test/java/ss/bshop/domain/ArticleDAOTest.java
@@ -17,11 +17,7 @@ import ss.bshop.domain.Article;
 import java.util.List;
 
 /**
- * MedicineDAO Tester. 
- *
- * @author <Authors name> 
- * @since <pre>��� 3, 2012</pre> 
- * @version 1.0
+ * ArticleDAO Tester. 
  */
 @ContextConfiguration("/test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -32,201 +28,43 @@ public class ArticleDAOTest {
     private ArticleDAO daoI;
 
     /**
-     *
-     * Method: save(Medicine medicine)
-     *
+     * Method: save(Article article)
      */
     @Test
-    public void testSave() throws Exception {
-//TODO: Test goes here...
-       Article article = new Article();
-       article.setName("Snicks");
-       article.setBarcode("831418518");
-       
-       System.out.println(article);
-      //Assert.assertEquals("Name","Name");
-       daoI.addArticle(article);
-        List<Article> articleList = daoI.getAll();
-        
-        for(Article a : articleList){
-        	System.out.println(a);
-        }
-        
-        
-       Assert.assertEquals(article,articleList.get(articleList.size()-1));
-    }
-//
-//    /**
-//     *
-//     * Method: load(String barCode)
-//     *
-//     */
-//    @Test
-//    public void testLoad() throws Exception {
-////TODO: Test goes here...
-//
-//        Medicine medicine = new Medicine("9999999999999","Name", mtDAOI.getList().get(0),false);
-//        daoI.save(medicine);
-//        Thread.sleep(500);
-//        List<Medicine> medicineList = daoI.getList();
-//        System.out.println("medicineList(n-1) component: "+medicineList.get(medicineList.size()-1).getName());
-//        Medicine testMedicine = daoI.load(medicineList.get(medicineList.size()-1).getBarCode());
-//        Assert.assertEquals("Name", testMedicine.getName());
-//
-//    }
-//    /**
-//     *
-//     * Method: GetListByBarCode(String barCode)
-//     *
-//     */
-//    @Test
-//    public void testGetListByBarCode() throws Exception
-//    {
-//        List<MedicineType> mtList = mtDAOI.getList();
-//        Medicine medicine1 = new Medicine("999999777","Test1",mtList.get(0),false);
-//        Medicine medicine2 = new Medicine("999999666","Test2",mtList.get(0),false);
-//        Medicine medicine3 = new Medicine("999999555","Test3",mtList.get(0),false);
-//        daoI.save(medicine1);
-//        daoI.save(medicine2);
-//        daoI.save(medicine3);
-//
-//        List<Medicine> medList = daoI.getListByBarCode("999999");
-//       /* for(Medicine m:medList)
-//        {
-//            System.out.println(m.toString());
-//        }   */
-//
-//        Assert.assertEquals(3,medList.size());
-//       
-////
-////    /**
-////     *
-////     * Method: load(String barCode)
-////     *
-////     */
-////    @Test
-////    public void testLoad() throws Exception {
-//////TODO: Test goes here...
-////
-////        Medicine medicine = new Medicine("9999999999999","Name", mtDAOI.getList().get(0),false);
-////        daoI.save(medicine);
-////        Thread.sleep(500);
-////        List<Medicine> medicineList = daoI.getList();
-////        System.out.println("medicineList(n-1) component: "+medicineList.get(medicineList.size()-1).getName());
-////        Medicine testMedicine = daoI.load(medicineList.get(medicineList.size()-1).getBarCode());
-////        Assert.assertEquals("Name", testMedicine.getName());
-////
-////    }
-////    /**
-////     *
-////     * Method: GetListByBarCode(String barCode)
-////     *
-////     */
-////    @Test
-////    public void testGetListByBarCode() throws Exception
-////    {
-////        List<MedicineType> mtList = mtDAOI.getList();
-////        Medicine medicine1 = new Medicine("999999777","Test1",mtList.get(0),false);
-////        Medicine medicine2 = new Medicine("999999666","Test2",mtList.get(0),false);
-////        Medicine medicine3 = new Medicine("999999555","Test3",mtList.get(0),false);
-////        daoI.save(medicine1);
-////        daoI.save(medicine2);
-////        daoI.save(medicine3);
-////
-////        List<Medicine> medList = daoI.getListByBarCode("999999");
-////       /* for(Medicine m:medList)
-////        {
-////            System.out.println(m.toString());
-////        }   */
-////
-////        Assert.assertEquals(3,medList.size());
-////       
-////
-////    }
-////    /**
-////     *
-////     * Method: getListByName(String name)
-////     *
-////     */
-////    @Test
-////    public void getListByName() throws Exception{
-////        List<MedicineType> mtList = mtDAOI.getList();
-////        Medicine medicine1 = new Medicine("999999777","TestName1",mtList.get(0),false);
-////        Medicine medicine2 = new Medicine("999999666","TestName2",mtList.get(0),false);
-////        Medicine medicine3 = new Medicine("999999555","TestName3",mtList.get(0),false);
-////        daoI.save(medicine1);
-////        daoI.save(medicine2);
-////        daoI.save(medicine3);
-////
-////        List<Medicine> medList = daoI.getListByName("TestName");
-////        /* for(Medicine m:medList)
-////     {
-////         System.out.println(m.toString());
-////     }   */
-////
-////        Assert.assertEquals(3,medList.size());
-////
-////    }
-////    /**
-////     *
-////     * Method: update(Medicine medicine)
-////     *
-////     */
-////    @Test
-////    public void testUpdate() throws Exception {
-//////TODO: Test goes here...
-////        Medicine medicine = new Medicine("123456789","Name", mtDAOI.getList().get(0),false);
-////        daoI.save(medicine);
-////        List<Medicine> medicineList = daoI.getList();
-////        Medicine toSave = medicineList.get(medicineList.size()-1);
-////        toSave.setName("NewNameAfterUpdate");
-////        daoI.update(toSave);
-////        medicineList = daoI.getList();
-////        Assert.assertEquals("NewNameAfterUpdate",medicineList.get(medicineList.size()-1).getName());
-////
-////    }
-////
-////    /**
-////     *
-////     * Method: delete(Medicine medicine)
-////     *
-////     */
-////    @Test
-////    public void testDelete() throws Exception {
-//////TODO: Test goes here...
-////        Medicine medicine = new Medicine("9999999999999","Name", mtDAOI.getList().get(0),false);
-////        daoI.save(medicine);
-////        List<Medicine> medicineList = daoI.getList();
-////        daoI.delete(medicineList.get(medicineList.size()-1));
-////        if(medicineList.size()> daoI.getList().size())
-////        {
-////            Assert.assertTrue(true);
-////        }
-////        
-////        
-////
-////    }
-////
-////    /**
-////     *
-////     * Method: getList()
-////     *
-////     */
-////    @Test
-////    public void testGetList() throws Exception {
-//////TODO: Test goes here...
-////        Medicine medicine = new Medicine("123456789","Name",new MedicineType(1,"Snotvornoe"),false);
-////        daoI.save(medicine);
-////        List<Medicine> medicineList = daoI.getList();
-////        if(medicineList.size()>0)
-////        {
-////            Assert.assertTrue(true);
-////        }
-////    }
-////
-////    @Test
-////    public void testEmptyOne()
-////    {
-////        org.junit.Assert.assertTrue(true);
-////    }
+	public void testSave() throws Exception {
+		Article article = new Article();
+		article.setName("Snicks");
+		article.setBarcode("831418518");
+
+		System.out.println(article);
+		daoI.addArticle(article);
+		List<Article> articleList = daoI.getAll();
+
+		// for(Article a : articleList){
+		// System.out.println(a);
+		// }
+
+		Assert.assertEquals(article, articleList.get(articleList.size() - 1));
+		Assert.assertEquals(article.getName(),
+				articleList.get(articleList.size() - 1).getName());
+	}
+    
+    /**
+     * Method: getArticle(Integer articleId)
+     */
+    @Test
+	public void testGetArticle() throws Exception {
+
+		Article article = new Article();
+		article.setName("S");
+
+		daoI.addArticle(article);
+		List<Article> articleList = daoI.getAll();
+
+		Article loadedArticle = articleList.get(articleList.size() - 1);
+		Long loadedArticleId = loadedArticle.getId();
+		Assert.assertEquals(loadedArticle, daoI.getArticle(loadedArticleId));
+	}
+    
+
 }  
