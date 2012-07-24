ddf05866b72106f66c1be86dcf5576c5e2859dec
nick <nick@eMachina>
2012-07-24 11:35:30 +0300

    смотри Outlet

--- src/main/java/ss/bshop/dao/IOutletDAO.java
+++ src/main/java/ss/bshop/dao/IOutletDAO.java
@@ -3,9 +3,20 @@ package ss.bshop.dao;
 import java.util.List;
 import ss.bshop.domain.SupplierOrder;
 import org.springframework.data.repository.CrudRepository;
+import ss.bshop.domain.Article;
 import ss.bshop.domain.Outlet;
 
 public interface IOutletDAO extends CrudRepository<Outlet, Long>{
-	List<Outlet> findByName(String name);
+	
+	
+        public void addOutlet(Outlet outlet);
+
+	public Outlet getOutlet(Long articleId);
+
+	public List<Article> getOutlet();
+
+	public void removeOutlet(Long id);
+	
+	public void updateArticle(Long id);
 
 }
