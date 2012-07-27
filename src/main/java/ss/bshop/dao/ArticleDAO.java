756ddd3c9f994be8a2079f603f64e0980c550082
Vic <tokarevv@yahoo.com>
2012-07-27 13:38:44 +0300

    1

--- src/main/java/ss/bshop/dao/ArticleDAO.java
+++ src/main/java/ss/bshop/dao/ArticleDAO.java
@@ -1,11 +1,15 @@
 package ss.bshop.dao;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import ss.bshop.domain.Article;
+import ss.bshop.domain.Supplier;
 
 @Repository
 public class ArticleDAO implements IArticleDAO {
@@ -42,6 +46,20 @@ public class ArticleDAO implements IArticleDAO {
 	@Override
 	public void update(Article article) {
 		sessionFactory.getCurrentSession().update(article);
+	}
+
+	@Override
+	public List<Article> getBySupplier(Supplier supplier) {
+		List<Article> res = new ArrayList<Article>();	
+		try {
+			Query q = sessionFactory.getCurrentSession().createQuery("from Article where supplier : supplier");
+			q.setParameter("supplier", supplier);
+			res = q.list();	
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return res;
 	}
 
 }
--- src/main/java/ss/bshop/dao/IArticleDAO.java
+++ src/main/java/ss/bshop/dao/IArticleDAO.java
@@ -3,7 +3,10 @@ package ss.bshop.dao;
 import java.util.List;
 
 import ss.bshop.domain.Article;
+import ss.bshop.domain.Supplier;
 
 public interface IArticleDAO extends ICRUDGeneral<Article, Long>{
+	
+	public List<Article> getBySupplier(Supplier supplier);
 
 }
