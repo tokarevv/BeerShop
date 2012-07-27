67469c9e2b9f4ecc4df1923cb630ca3a63e42cf7
Vic <tokarevv@yahoo.com>
2012-07-27 12:55:05 +0300

    SupOrderMB and stuff

---
+++ src/main/java/ss/bshop/mbeans/SupplierOrderManagedBean.java
@@ -0,0 +1,47 @@
+package ss.bshop.mbeans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+import ss.bshop.domain.Supplier;
+import ss.bshop.service.ISupplierService;
+
+@ManagedBean(name="supplierOrderMB")
+@RequestScoped
+public class SupplierOrderManagedBean{
+
+	@ManagedProperty(value="#{supplierService}")
+	ISupplierService supplierService;
+	
+	private Supplier selected;
+	
+	private String orderType;
+	
+	public Supplier getSelected() {
+		return selected;
+	}
+
+	public void setSelected(Supplier selected) {
+		this.selected = selected;
+	}
+
+	public ISupplierService getSupplierService() {
+		return supplierService;
+	}
+
+	public void setSupplierService(ISupplierService supplierService) {
+		this.supplierService = supplierService;
+	}
+
+	public String getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+}
