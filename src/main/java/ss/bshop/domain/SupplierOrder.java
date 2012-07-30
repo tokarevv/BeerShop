6640c512bb514a2b21521d5abab11007ba092b2b
Verenok <didkovskayavera@gmail.com>
2012-07-30 12:40:59 +0300

    1

--- src/main/java/ss/bshop/domain/SupplierOrder.java
+++ src/main/java/ss/bshop/domain/SupplierOrder.java
@@ -2,12 +2,14 @@ package ss.bshop.domain;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -37,7 +39,7 @@ public class SupplierOrder {
 	@Column
 	private String orderDoneDate;
 	
-	@Column
+	@OneToOne(optional=false, cascade=CascadeType.ALL)
 	private Supplier supplier;
 	
 	@Column
