634b6c7624a61ed15fbae788da75a4f6dff46fc0
nick <nick@eMachina>
2012-08-07 09:25:03 +0300

    merge

--- src/main/java/ss/bshop/mbeans/SalesRepMB.java
+++ src/main/java/ss/bshop/mbeans/SalesRepMB.java
@@ -71,13 +71,15 @@ public class SalesRepMB implements Serializable {
         
         outletList = outletService.getBySalesRep(selected);
         for(Outlet outlet: outletList){
-
-            LatLng coord = new LatLng(outlet.getLatitude(),outlet.getLongitude());
             
-            mapModel.addOverlay(new Marker(coord));
-            s+=coord+";\r\n";
+            if(outlet.getLatitude()!=outlet.getLongitude()){
+                LatLng coord = new LatLng(outlet.getLatitude(),outlet.getLongitude());
+                mapModel.addOverlay(new Marker(coord));
+                s+=coord+";\r\n";
+            }
         }
-         msg = new FacesMessage("Coords", s);   
+        
+        msg = new FacesMessage("Coords", s);   
         FacesContext.getCurrentInstance().addMessage(null, msg); 
 
     }
